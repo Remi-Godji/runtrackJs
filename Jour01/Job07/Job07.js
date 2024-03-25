af18f62141b775b7d7beff81bcc17056eff7f8c4
Remi-godji <146717466+Remi-Godji@users.noreply.github.com>
2024-03-25 23:31:09 +0100

    finish

--- Jour01/Job07/Job07.js
+++ Jour01/Job07/Job07.js
@@ -1,20 +1,31 @@
 function jourTravaille(date) {
     var annee = date.getFullYear();
-    var mois = date.getMonth() + 1; // Les mois commencent à partir de 0
+    var mois = date.getMonth() + 1; // Les mois commencent à 0, donc on ajoute 1
     var jour = date.getDate();
-    var joursFeries = ["2024-01-01", "2024-04-01", "2024-05-01", "2024-05-08", "2024-05-30", "2024-07-14", "2024-08-15", "2024-11-01", "2024-11-11", "2024-12-25"]; // Dates des jours fériés en 2024
+    var joursFeries = [
+        "2024-01-01", // Nouvel An
+        "2024-04-01", // Fête du Travail
+        "2024-05-08", // Victoire des Alliés
+        "2024-07-14", // Fête Nationale
+        "2024-08-15", // Assomption
+        "2024-11-01", // Toussaint
+        "2024-11-11", // Armistice
+        "2024-12-25"  // Noël
+    ];
 
-    var dateStr = annee + '-' + mois.toString().padStart(2, '0') + '-' + jour.toString().padStart(2, '0');
+    var jourSemaine = date.getDay(); // 0 pour Dimanche, 1 pour Lundi, ..., 6 pour Samedi
+
+    var dateStr = date.toISOString().slice(0,10);
 
     if (joursFeries.includes(dateStr)) {
-        console.log("Le " + jour + "/" + mois + "/" + annee + " est un jour férié.");
-    } else if (date.getDay() === 6) {
-        console.log("Le " + jour + "/" + mois + "/" + annee + " est un samedi.");
+        console.log("Le " + jour + "/" + mois + "/" + annee + " est un jour férié");
+    } else if (jourSemaine === 0 || jourSemaine === 6) {
+        console.log("Non, le " + jour + "/" + mois + "/" + annee + " est un week-end");
     } else {
-        console.log("Le " + jour + "/" + mois + "/" + annee + " est un jour travaillé.");
+        console.log("Oui, le " + jour + "/" + mois + "/" + annee + " est un jour travaillé");
     }
 }
 
-// Exemple d'utilisation de la fonction avec une date spécifique
-var dateTest = new Date("2024-06-01");
+// Exemple d'utilisation de la fonction jourTravaille
+var dateTest = new Date("2024-04-01"); // 1er avril 2024
 jourTravaille(dateTest);
