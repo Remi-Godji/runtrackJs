705fb981ff621544fbafc8f8b5f0d627c6e36c79
Remi-godji <146717466+Remi-Godji@users.noreply.github.com>
2024-03-25 12:36:39 +0100

    finishing

---
+++ Jour01/Job05/Job05.js
@@ -0,0 +1,12 @@
+function afficherJoursSemaines() {
+    // Création du tableau contenant les jours de la semaine
+    var joursSemaines = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+
+    // Boucle for pour afficher chaque jour de la semaine
+    for (var i = 0; i < joursSemaines.length; i++) {
+        console.log(joursSemaines[i]);
+    }
+}
+
+// Appel de la fonction pour afficher les jours de la semaine
+afficherJoursSemaines();
